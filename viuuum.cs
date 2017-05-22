3b2c7264b94c449d64667be821c17d2dd1114170
dark1knight <nakamuta26@gmail.com>
2017-05-22 16:53:38 -0300

    Add files via upload

---
+++ viuuum.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class viuuum : MonoBehaviour {
+    public AudioSource fiuu;
+	// Use this for initialization
+	void Start () {
+        fiuu = gameObject.GetComponent<AudioSource>();     
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
